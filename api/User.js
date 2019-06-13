0b4077f9fe4ab21bd5021813c660856f6e3d91ef
Johannes Kettmann <jkettmann@gmx.net>
2019-06-13 19:38:30 +0200

    Add mock DB model

---
+++ api/User.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    id: '1',
+    firstName: 'Maurice',
+    lastName: 'Moss',
+    email: 'maurice@moss.com',
+    password: 'abcdefg'
+  },
+  {
+    id: '2',
+    firstName: 'Roy',
+    lastName: 'Trenneman',
+    email: 'roy@trenneman.com',
+    password: 'imroy'
+  }
+];
+
+export default {
+  getUsers: () => users,
+  addUser: (user) => users.push(user),
+};
