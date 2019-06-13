e168df8aeb9815944868099945396315636f95ca
Johannes Kettmann <jkettmann@gmx.net>
2019-06-13 19:38:14 +0200

    Setup express-session

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import session from 'express-session';
+import uuid from 'uuid/v4';
 
 const PORT = 4000;
+const SESSION_SECRECT = 'bad secret';
 
 const app = express();
+
+app.use(session({
+  genid: (req) => uuid(),
+  secret: SESSION_SECRECT,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.listen({ port: PORT }, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
