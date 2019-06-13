82adee91715ba3e1cd8ac1f724ce864c21855dd3
Johannes Kettmann <jkettmann@gmx.net>
2019-06-13 19:41:41 +0200

    Setup passport

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import session from 'express-session';
 import uuid from 'uuid/v4';
+import passport from 'passport';
 
 const PORT = 4000;
 const SESSION_SECRECT = 'bad secret';
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  const users = User.getUsers();
+  const matchingUser = users.find(user => user.id === id);
+  done(null, matchingUser);
+});
 
 const app = express();
 
@@ -13,6 +24,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen({ port: PORT }, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
